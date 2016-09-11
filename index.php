65aa7f33c7b0b7e20d60dad8bdae1c9a86c0ff27
Hexagoo <fevre.nico@hotmail.fr>
2016-09-11 11:54:38 +0200

    Afficher le nbr de participants

---
+++ index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+  </head>
+
+  <?php include_once("../participants.php") ?>
+
+  <body>
+  	<?php $participants ?>
+
+  </body>
+
+</html>
